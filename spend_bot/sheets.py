9dfc99dc787bd20c91f92b5f44321499239bdff8
Dmitrii Morozov <33morozov@gmail.com>
2022-08-07 13:40:07 +0300

    Fixed addres Gsheet

--- spend_bot/sheets.py
+++ spend_bot/sheets.py
@@ -6,7 +6,7 @@ from datetime import datetime
 def save_to_sheet(data):
     LINK_SHEET = os.getenv('LINK_SHEET')
     gc = pygsheets.authorize(service_account_env_var = 'GDRIVE_API_CREDENTIALS')
-    worksheet = gc.open_by_url(LINK_SHEET).sheet1
+    worksheet = gc.open_by_url(LINK_SHEET).worksheet_by_title('Траты')
     data['datetime'] = datetime.today().strftime('%d.%m.%Y %H:%M')
     record = []
     record.append(data.get('datetime'))
